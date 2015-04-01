8ab5e31f4e2780e5be9656224703f520b2875e3c
Curtis McEnroe <programble@gmail.com>
2015-04-01 17:32:43 -0400

    Add salt column to vault table

--- migrations/i7yxy7p6.create-vault.up.sql
+++ migrations/i7yxy7p6.create-vault.up.sql
@@ -2,6 +2,7 @@ CREATE EXTENSION "uuid-ossp";
 CREATE TABLE vault (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   key_id text NOT NULL,
+  salt bytea NOT NULL,
   data bytea NOT NULL,
   created_at timestamp NOT NULL DEFAULT now(),
   updated_at timestamp NOT NULL DEFAULT now(),
